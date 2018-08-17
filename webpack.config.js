1ef67ce7f6898a4f3926e12d10156dedfa70d86c
Alex <muban96@mail.ru>
2018-08-17 10:38:33 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: {
+        "responsive": "./src/js/index.js",
+        "responsive.min": "./src/js/index.min.js"
+    },
+    devtool: "source-map",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: "eslint-loader"
+            }]
+        },
+        {
+            test: /\.min\.scss$/,
+            use: ExtractTextPlugin.extract({
+                use: [
+                    {
+                        loader: "css-loader",
+                        options: { sourceMap: true, minimize: true }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: { sourceMap: true }
+                    }
+                ]
+            })
+        },
+        {
+            test: /[^\.min]\.scss$/,
+            use: ExtractTextPlugin.extract({
+                use: [
+                    {
+                        loader: "css-loader",
+                        options: { sourceMap: true, minimize: false }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: { sourceMap: true }
+                    }
+                ]
+            })
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin("[name].css"),
+        new UglifyJSPlugin({
+            test: /\.min\.js$/,
+            sourceMap: true
+        })
+    ]
+};
